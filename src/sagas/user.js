d977d8a59727bda59b084913f3917f34c679571c
erwanledoux <lawanledoux@gmail.com>
2017-09-08 00:53:16 +0200

    fixed saga

--- src/sagas/user.js
+++ src/sagas/user.js
@@ -10,25 +10,19 @@ import { getAuthorizedLinks,
 } from 'transactions-authorization-state'
 import { IS_UNDER_CONSTRUCTION,
   getLocationSearch } from 'transactions-interface-state'
+import { isSuccessTransactionsAction,
+  request } from 'transactions-redux-request'
 import { mergeReselector } from 'transactions-redux-reselector'
-import { isSuccessTransactionsAction } from 'transactions-redux-request'
 
 import { SET_USER } from '../reducers/user'
 
 // DATA
 function * fromWatchSetUserData (action) {
-  const { config: { getFilteredElements,
-      joinedModeCollectionNames,
-      requestTransactions
-    },
+  const { config: { joinedModeCollectionNames },
     user: { active,
       id
     }
   } = action
-  if (!requestTransactions) {
-    console.warn('transactions-interface-state fromWatchSetUserData, you need to pass a requestTransactions in your action')
-    return
-  }
   if (active) {
     // say to the socket server that we are connected as a user !
     if (action.socketio) {
@@ -36,7 +30,7 @@ function * fromWatchSetUserData (action) {
     }
     // now we need to get all the joined collections
     // that say more about the user... Is she/he a reviewer, an editor...?
-    yield put(requestTransactions('GET',
+    yield put(request('GET',
       [{
         collectionName: 'users',
         horizontals: [
@@ -49,7 +43,6 @@ function * fromWatchSetUserData (action) {
         query: { id }
       }],
       {
-        extra: { getFilteredElements },
         tag: 'sign'
       }
     ))
@@ -98,7 +91,7 @@ function * fromWatchMergeNormalizerGetSignActionData (action) {
     console.warn(`there should be a patch here to determine if we have
       merged the logged user`, action)
   }
-  const authorizedModes = yield select(getAuthorizedModes, action.extra.getFilteredElements)
+  const authorizedModes = yield select(getAuthorizedModes)
   const authorizedLinks = getAuthorizedLinks(authorizedModes)
   yield put(setAuthorizationLinks(authorizedLinks, authorizedModes))
 }
